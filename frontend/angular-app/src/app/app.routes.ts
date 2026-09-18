cd9b032116073f0f28d04c3308d45a9d8d4252c1
Mohammad Abdul Aziz <m.a.aziz1900@gmail.com>
2026-09-18 09:52:49 +0200

    feat: initialize Angular admin application

---
+++ frontend/angular-app/src/app/app.routes.ts
@@ -0,0 +1,14 @@
+import { Routes } from '@angular/router';
+import { Dashboard } from './pages/dashboard/dashboard';
+
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'dashboard',
+    pathMatch: 'full',
+  },
+  {
+    path: 'dashboard',
+    component: Dashboard,
+  },
+];
